1c858f2c079386e74aff83d9af0ca7f795f8b722
jhawkins11 <josiah.c.hawkins@gmail.com>
2023-01-30 10:40:29 -0600

    add map copmonent

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { Company } from './Company';
-import { User } from './User';
+/// <reference types="@types/google.maps" />
+// import { Company } from './Company';
+// import { User } from './User';
 
-const user = new User();
-
-const company = new Company();
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
